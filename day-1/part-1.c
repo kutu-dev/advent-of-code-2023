eb7640c689388c75d34bffb5388c459d3f32fdb5
Kutu <git@dobon.dev>
2023-12-02 01:56:32 +0100

    Add part 1 of day 1

---
+++ day-1/part-1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *input_file;
+
+    if ((input_file = fopen("input.txt", "r")) == NULL) {
+        printf("File could not be opened");
+        return 1;
+    }
+
+    char current_char;
+    int lines = 0;
+
+    while ( current_char != EOF ) {
+        if ( current_char == '\n' ) {
+            lines++;
+        }
+        current_char = getc(input_file);
+    }
+
+    rewind(input_file);
+
+    
+    char *bad_calibration_values[lines];
+
+    for (int i = 0; i < lines; i++) {
+        bad_calibration_values[i] = NULL;
+        size_t line_buffer_size = 0;
+        
+        getline(&bad_calibration_values[i], &line_buffer_size, input_file);
+    }
+
+    fclose(input_file);
+
+    int final_calibration_value = 0;
+
+    for (int i = 0; i < sizeof(bad_calibration_values)/sizeof(bad_calibration_values[0]); i++) {
+        int value_length = strlen(bad_calibration_values[i]);
+        char first_digit;
+        char last_digit;
+
+        for (int j = 0; j < value_length; j++) {
+            if (isdigit(bad_calibration_values[i][j])) {
+                first_digit = bad_calibration_values[i][j];
+                break;
+            }
+        }
+
+        for (int j = value_length; j >= 0; j--) {
+            if (isdigit(bad_calibration_values[i][j])) {
+                last_digit = bad_calibration_values[i][j];
+                break;
+            }
+        }
+
+        char fixed_calibration_value[] = {first_digit, last_digit, '\0'};
+        final_calibration_value += strtol(fixed_calibration_value, NULL, 10);
+    }
+
+    printf("%d\n", final_calibration_value);
+
+    return 0;
+}
