f887f1c3ef8b83addba41f4ae68c1319533852a9
Kutu <git@dobon.dev>
2023-12-06 12:39:16 +0100

    Add part 2 and bis of day 5

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC:=gcc
-CFLAGS:=-Wall -Wextra -Werror -Wpedantic -O2 -lm -ftrapv
+CFLAGS:=-Wall -Wextra -Werror -Wpedantic -O3
 BUILD:=./build
 OUT_FILENAMES:=p1d1 p2d1 p1d2 p2d2
 OUT_FILENAMES:=$(foreach out_filename,$(OUT_FILENAMES),$(out_filename).out)
@@ -65,6 +65,11 @@ p1d5.out: day-5/part-1.c common/logger.c
 	@$(CC) $(CFLAGS) $^ -o $(BUILD)/day-5/$@
 
 p2d5.out: day-5/part-2.c common/logger.c
+	@mkdir -p $(BUILD)/day-5
+	@cp day-5/input.txt $(BUILD)/day-5
+	@$(CC) $(CFLAGS) $^ -o $(BUILD)/day-5/$@
+
+p2d5b.out: day-5/part-2-bis.c common/logger.c
 	@mkdir -p $(BUILD)/day-5
 	@cp day-5/input.txt $(BUILD)/day-5
 	@$(CC) $(CFLAGS) $^ -o $(BUILD)/day-5/$@
--- day-5/part-2.c
+++ day-5/part-2.c
@@ -3,77 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-void transform_source_to_destination(char **input_lines,
-                                     int input_lines_length,
-                                     int map_index,
-                                     long long int *source_array,
-                                     int num_of_elements_in_source_array)
+long long transform_source_to_destination(char **input_lines, int input_lines_length, long long int source_number, char *buffered_line)
 {
-    printf(input_lines[map_index]);
-    printf(" %lld\n", source_array[0]);
-
-    if (input_lines_length < 0)
+    for (size_t i = 2; i < (size_t)input_lines_length; i++)
     {
-        return;
-    }
-
-    if (num_of_elements_in_source_array < 0)
-    {
-        return;
-    }
-
-    bool *indexes_already_transformed =
-            malloc(num_of_elements_in_source_array * sizeof(bool));
-        memset(indexes_already_transformed, false, num_of_elements_in_source_array);
-
-    // If the line is not empty and it's not the last line
-    for (size_t i = map_index + 1;
-         i < (size_t)input_lines_length && strcmp(input_lines[i], "") != 0;
-         i++)
-    {
-        long long int destination_range_start = strtol(strtok(input_lines[i], " "), NULL, 10);
-        long long int source_range_start = strtol(strtok(NULL, " "), NULL, 10);
-        long long int map_range_length = strtol(strtok(NULL, " "), NULL, 10);
-
-        long long int source_range_end = source_range_start + map_range_length - 1;
-
-        printf("LINE %ld\n", i+1);
-        printf("D%lld S%lld R%lld\n",
-               destination_range_start,
-               source_range_start,
-               map_range_length);
-        
-        for (size_t j = 0; j < (size_t)num_of_elements_in_source_array; j++)
+        if (strstr(input_lines[i], "map") == NULL)
         {
-            //printf("%lld\n", source_array[j]);
-            //printf("%d\n", indexes_already_transformed[j]);
+            continue;
+        }
 
-            if (indexes_already_transformed[j] == true)
+        //printf("NEW MAP %s\n", input_lines[i]);
+        bool already_changed = false;
+
+        for (size_t j = i + 1;
+         j < (size_t)input_lines_length && strcmp(input_lines[j], "") != 0;
+         j++)
+        {
+            if (already_changed) {
+                //printf("ALREADY CHANGED");
+                break;
+            }
+
+            strcpy(buffered_line, input_lines[j]);
+
+            long long int destination_range_start = strtol(strtok(buffered_line, " "), NULL, 10);
+            long long int source_range_start = strtol(strtok(NULL, " "), NULL, 10);
+            long long int map_range_length = strtol(strtok(NULL, " "), NULL, 10);
+
+            long long int source_range_end = source_range_start + map_range_length - 1;
+
+            //printf("LINE %ld\n", j+1);
+            //printf("D%lld S%lld R%lld\n",
+            //   destination_range_start,
+            //   source_range_start,
+            //   map_range_length);
+        
+            //printf("%lld\n", source_number);
+            //printf("%lld\n", source_range_end);
+
+
+            if (source_number < source_range_start ||
+                source_number > source_range_end)
             {
-                puts("ALREADY CHANGED!");
+                //printf("SKIPPED: %lld is bigger/smaller than %lld/%lld\n", source_number, source_range_end, source_range_start);
                 continue;
             }
 
-            if (source_array[j] < source_range_start ||
-                source_array[j] > source_range_end)
-            {
-                printf("SKIPPED: %lld is bigger/smaller than %lld/%lld\n", source_array[j], source_range_end, source_range_start);
-                continue;
-            }
-
-            printf("------------>>>>>> CHANGED %lld because it's in range %lld-%lld\n",source_array[j], source_range_start, source_range_end);
-
-            printf("NEW VALUE %lld\n", source_array[j]);
-            //printf("NEW VALUE %lld\n", source_range_start);
-            source_array[j] = destination_range_start + (source_array[j] - source_range_start);
-            //printf("NEW VALUE %lld\n", destination_range[source_array[j] - source_range_start]);
-            printf("NEW VALUE %lld\n", source_array[j]);
-
-            indexes_already_transformed[j] = true;
+            
+            source_number = destination_range_start + (source_number - source_range_start);
+            already_changed = true;
+            //printf("NUMBER CHANGED TO %lld\n", source_number);
         }
     }
-
-    free(indexes_already_transformed);
+    return source_number;
 }
 
 int main()
@@ -140,30 +122,30 @@ int main()
         seeds[i] = strtol(strtok(NULL, " "), NULL, 10);
     }
 
-    // Start at 2 as the two first lines will never be a map
-    for (size_t i = 2; i < lines; i++)
-    {
-        if (strstr(input_lines[i], "map") == NULL)
-        {
-            continue;
+    //long long int lowest_number = NULL;
+
+    long long int lowest_number = 0;
+
+    char *buffered_line = malloc(50);
+
+    for (size_t i = 0; i < num_of_seeds; i +=2) {
+        for (long long int j = seeds[i]; j < seeds[i] + seeds[i+1]; j++) {
+            
+            printf("PABAJO %lld ----------------------------------------\n", j);
+            long long int new_transformed_number = transform_source_to_destination(input_lines, lines, j, buffered_line);
+            printf("TRANSFORMADO A %lld\n", new_transformed_number);
+
+            if (lowest_number == 0 || new_transformed_number < lowest_number) {
+                lowest_number = new_transformed_number;
+                printf(">>>>>>>>>>>>>>>>>>> NUEVO MIN %lld\n", lowest_number);
+            }
+
+            //long long int transformed_number = 
+            //printf("%lld\n", transformed_number);
         }
-
-        transform_source_to_destination(input_lines, lines, i, seeds, num_of_seeds);
     }
-
-    for (size_t i = 0; i < num_of_seeds; i++) {
-        printf("%ld - %lld\n",i+1, seeds[i]);
-    }
-
-    long long int lowest_number = seeds[0];
-
-    for (size_t i = 1; i < num_of_seeds; i++) {
-        if (seeds[i] < lowest_number) {
-            lowest_number = seeds[i];
-        }
-    }
-
-    printf("SOLUTION %lld", lowest_number);
 
     free(seeds);
+    free(buffered_line);
+    printf("SOLUCION %lld\n", lowest_number);
 }
